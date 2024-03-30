18aae3ac2e8653ff7f81d42a592f432148f0c3c8
Gaazeon <317482944+gaezon@users.noreply.github.com>
2024-03-30 17:28:28 +0800

    Remove unused files

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/about_CAogZIRg.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/about_BFeNqEK5.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/pages/og_Bw-o9NhQ.mjs
+++
@@ -1,7 +0,0 @@
-import { g as generateOgImageForSite } from './index_BOXU35rr.mjs';
-
-const GET = async () => new Response(await generateOgImageForSite(), {
-  headers: { "Content-Type": "image/png" }
-});
-
-export { GET };
