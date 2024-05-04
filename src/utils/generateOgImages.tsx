2481cf3e5854fafcd8432ee2f133fa2c8f524c70
Gaazeon <317482944+gaezon@users.noreply.github.com>
2024-05-04 15:56:05 +0800

    Update font URLs in generateOgImages.tsx

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -7,13 +7,13 @@ import siteOgImage from "./og-templates/site";
 const fetchFonts = async () => {
   // Regular Font
   const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf"
+    "https://fonts.gaazeon.com/fonts%2FOPPOSans-Regular.ttf"
   );
   const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
 
   // Bold Font
   const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf"
+    "https://fonts.gaazeon.com/fonts%2FOPPOSans-Regular.ttf"
   );
   const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
 
