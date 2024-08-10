e9054de6c265da8bf1267bb42bf5e16a21a14962
Gaazeon <317482944+gaezon@users.noreply.github.com>
2024-08-10 11:26:00 +0800

    chore: Update Astro version to 4.13.3

--- .vercel/output/static/_astro/hoisted.DLmqwO-G.js
+++
@@ -1,6 +0,0 @@
-import"./hoisted.hebMR3Th.js";document.addEventListener("astro:page-load",()=>{function e(){if(document.getElementById("tcomment")){const t=document.createElement("script");t.src="https://cdn.staticfile.org/twikoo/1.6.39/twikoo.all.min.js",t.async=!0,t.onload=()=>{const n=document.createElement("script");n.innerHTML=`
-            twikoo.init({
-                envId: 'https://comment.gaazeon.com/',
-                el: '#tcomment',
-            });
-        `,document.body.appendChild(n)},document.body.appendChild(t)}}e()});
