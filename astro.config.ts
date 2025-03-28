d62d964716ac5107d4e84d3628773ba58d30a34a
Gaazeon <317482944+gaezon@users.noreply.github.com>
2025-03-28 10:40:31 +0800

    添加 @vercel/speed-insights 依赖并在 astro.config.ts 中配置以优化页面加载速度。

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
+import speedInsights from '@vercel/speed-insights/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
+    speedInsights(),
   ],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
