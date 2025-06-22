f4ae57d8264382b39adfbfd77f7e62fe6686ddae
Gaazeon <317482944+gaezon@users.noreply.github.com>
2025-06-22 21:19:43 +0800

    fix: use inline-svg strategy for mermaid

--- astro.config.ts
+++ astro.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     rehypePlugins: [[
       rehypeMermaid,
       {
-        strategy: "inline",
+        strategy: "inline-svg",
         dark: true,
         colorScheme: "light",
         mermaidConfig: {
