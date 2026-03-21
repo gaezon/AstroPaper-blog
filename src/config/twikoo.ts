4a5d43c1e2f3cd93b4e0acb68206babb1688ee6d
Gaazeon <317482944+gaezon@users.noreply.github.com>
2026-03-21 09:43:16 +0800

    chore(twikoo): upgrade to 1.7.4

--- src/config/twikoo.ts
+++ src/config/twikoo.ts
@@ -1,3 +1,3 @@
-export const TWIKOO_VERSION = "1.7.3";
+export const TWIKOO_VERSION = "1.7.4";
 export const TWIKOO_CDN_URL = `https://cdn.jsdelivr.net/npm/twikoo@${TWIKOO_VERSION}/dist/twikoo.min.js`;
 export const TWIKOO_CSS_URL = `https://cdn.jsdelivr.net/npm/twikoo@${TWIKOO_VERSION}/dist/twikoo.css`;
