c7f0a29266bfca373eef6a0101c3560816d3482d
Gaazeon <317482944+gaezon@users.noreply.github.com>
2026-09-25 15:51:00 +0800

    chore(twikoo): upgrade frontend to 2.0.9

--- src/config/twikoo.ts
+++ src/config/twikoo.ts
@@ -1,4 +1,4 @@
-export const TWIKOO_VERSION = "2.0.8";
+export const TWIKOO_VERSION = "2.0.9";
 export const TWIKOO_CDN_URL = `https://cdn.jsdelivr.net/npm/twikoo@${TWIKOO_VERSION}/dist/twikoo.min.js`;
 // Served from public/twikoo/owo.json. comment.gaazeon.com GET_CONFIG sets EMOTION_CDN to this path.
 export const TWIKOO_EMOTION_CDN = "/twikoo/owo.json";
